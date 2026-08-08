82723a16c00a1b9049407e9c03866f01ebe7bc61
Mayaz120102 <abrarmayaz2002@gmail.com>
2026-08-09 00:52:45 +0600

    login done

--- router/auth.py
+++ router/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import JSONResponse
 from typing import Annotated
 from datetime import datetime, timedelta, timezone
@@ -8,11 +8,18 @@ from sqlalchemy.orm import Session
 from models import Users
 from database import SessionLocal
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 
 
 router = APIRouter()
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+SECRET_KEY = "7a0b409366e78720240c6d21f0b4b145600221b4c7ceab96fcb2a0c28e029cb2"
+
+ALGORITHM = "HS256"
 
 
 class CreateUser(BaseModel):
@@ -25,6 +32,27 @@ class UserResponse(BaseModel):
     id: int
     username: str
     email: str
+
+
+def authenticate_user(username, password, db):
+    user = db.query(Users).filter(Users.username == username).first()
+
+    if user is None:
+        return False
+
+    if bcrypt_context.verify(password, user.hashed_password):
+        return user
+
+    return False
+
+
+def create_access_token(username: str, user_id: int, expire_delta: timedelta):
+    encode = {"sub": username, "id": user_id}
+    expires = datetime.now(timezone.utc) + expire_delta
+
+    encode.update({"exp": expires})
+
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
 def get_db():
@@ -51,3 +79,18 @@ def register_user(db: db_dependency, new_user: CreateUser):
     db.refresh(user_model)
 
     return user_model
+
+
+@router.post("/auth/login")
+def login_user(
+    db: db_dependency, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+
+    user = authenticate_user(form_data.username, form_data.password, db)
+
+    if not user:
+        raise HTTPException(status_code=401, detail="Failed Authentication")
+
+    token = create_access_token(user.username, user.id, timedelta(minutes=30))
+
+    return {"access_token": token, "token_type": "bearer"}
