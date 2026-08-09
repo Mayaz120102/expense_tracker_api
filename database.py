686bf3849a23215e56725730971680b562329e3f
Mayaz120102 <abrarmayaz2002@gmail.com>
2026-08-09 17:42:53 +0600

    supabase added

--- database.py
+++ database.py
@@ -2,14 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
 # for sqlite3
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./expensetracker.db"
 
 # for postgressql local
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql://postgres:password@localhost/ExpenseTrackerDatabase"
-)
+# SQLALCHEMY_DATABASE_URL = (
+#     "postgresql://postgres:password@localhost/ExpenseTrackerDatabase"
+# )
+
+# for supabase
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.girhvaihwupzluuqrmbo:abrarmayaz758@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
